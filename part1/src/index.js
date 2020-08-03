1e3213ee1d0ce040aad6471fc8149179ee751838
Joshua Herrera <joshuah@zbrains.net>
2020-08-03 12:12:02 -0700

    destructinr

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,11 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  // console.log("Hello from component");
+const Hello = ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age;
+
   return (
     <div>
-      <p>Hello world</p>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const name = "Joshua";
+  const age = 28;
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={27 + 10} />
+      <Hello name={name} age={age} />
     </div>
   );
 };
