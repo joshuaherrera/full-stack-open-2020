ae413820df137da48ceb6179a7cbaad2fe4936e0
Joshua Herrera <joshuah@zbrains.net>
2020-08-05 14:56:59 -0700

    array usage

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,26 +1,27 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const Display = ({ counter }) => {
-  return <div>{counter}</div>;
-};
-
-const Button = ({ handleClick, text }) => {
-  return <button onClick={handleClick}>{text}</button>;
-};
-
-const App = () => {
-  const [counter, setCounter] = useState(0);
-  const decreaseByOne = () => setCounter(counter - 1);
-  const increaseByOne = () => setCounter(counter + 1);
-  const setToZero = () => setCounter(0);
-
+const App = (props) => {
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    setLeft(left + 1);
+  };
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    setRight(right + 1);
+  };
   return (
     <div>
-      <Display counter={counter} />{" "}
-      <Button handleClick={increaseByOne} text="plus" />{" "}
-      <Button handleClick={setToZero} text="zero" />{" "}
-      <Button handleClick={decreaseByOne} text="minus" />
+      <div>
+        {left}
+        <button onClick={handleLeftClick}>left</button>
+        <button onClick={handleRightClick}>right</button>
+        {right}
+        <p>{allClicks.join(" ")}</p>{" "}
+      </div>
     </div>
   );
 };
