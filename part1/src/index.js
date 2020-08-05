fcea5567ec988d5c8438b32842e4357a204270b9
Joshua Herrera <joshuah@zbrains.net>
2020-08-05 09:31:18 -0700

    on complex state section

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,28 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age;
+const Display = ({ counter }) => {
+  return <div>{counter}</div>;
+};
 
-  return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  );
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
 };
 
 const App = () => {
-  const name = "Joshua";
-  const age = 28;
+  const [counter, setCounter] = useState(0);
+  const decreaseByOne = () => setCounter(counter - 1);
+  const increaseByOne = () => setCounter(counter + 1);
+  const setToZero = () => setCounter(0);
 
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={27 + 10} />
-      <Hello name={name} age={age} />
+      <Display counter={counter} />{" "}
+      <Button handleClick={increaseByOne} text="plus" />{" "}
+      <Button handleClick={setToZero} text="zero" />{" "}
+      <Button handleClick={decreaseByOne} text="minus" />
     </div>
   );
 };
