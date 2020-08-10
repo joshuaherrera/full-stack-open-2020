31b0952a530ae01541dbaa8b0e1958f0234a1984
Joshua Herrera <joshuah@zbrains.net>
2020-08-10 09:46:49 -0700

    start part 2

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,21 +1,25 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+const Display = (props) => <div>{props.value}</div>;
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
+
 const App = (props) => {
   const [value, setValue] = useState(10);
 
-  const hello = (who) => {
-    const handler = () => {
-      console.log("hello", who);
-    };
-    return handler;
+  const setToValue = (newValue) => {
+    setValue(newValue);
   };
+
   return (
     <div>
-      {value}
-      <button onClick={hello("world")}>button</button>
-      <button onClick={hello("react")}>button</button>
-      <button onClick={hello("function")}>button</button>{" "}
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
   );
 };
