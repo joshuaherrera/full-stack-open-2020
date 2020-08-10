9194264d943836d223b645c998925f0b25bb47b4
Joshua Herrera <joshuah@zbrains.net>
2020-08-10 09:27:08 -0700

    practcal usage of returnd functions in event handler

--- part1/src/index.js
+++ part1/src/index.js
@@ -4,14 +4,18 @@ import ReactDOM from "react-dom";
 const App = (props) => {
   const [value, setValue] = useState(10);
 
-  const hello = () => {
-    const handler = () => console.log("hello world");
+  const hello = (who) => {
+    const handler = () => {
+      console.log("hello", who);
+    };
     return handler;
   };
   return (
     <div>
       {value}
-      <button onClick={hello()}>button</button>
+      <button onClick={hello("world")}>button</button>
+      <button onClick={hello("react")}>button</button>
+      <button onClick={hello("function")}>button</button>{" "}
     </div>
   );
 };
