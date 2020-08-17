5abb7961666e8b70ddbd8815dc4e718a5cfc4971
Joshua Herrera <joshuah@zbrains.net>
2020-08-17 11:44:53 -0700

    finished 2.14 on 2d

--- submission/part2/countries/src/components/Country.js
+++ submission/part2/countries/src/components/Country.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Country = ({ country }) => {
+  const [weather, setWeather] = useState({});
+
+  // https://weatherstack.com/quickstart
+  const weatherKey = process.env.REACT_APP_API_KEY;
+  const params = {
+    access_key: weatherKey,
+    query: country.name,
+  };
+  const hook = () => {
+    axios.get("http://api.weatherstack.com/current", { params }).then((res) => {
+      setWeather(res.data.current);
+    });
+  };
+
+  useEffect(hook, []);
   return (
     <>
       <h2>{country.name}</h2>
@@ -13,6 +29,20 @@ const Country = ({ country }) => {
         ))}
       </ul>
       <img src={country.flag} alt="Flag" width="275" height="200" />
+      <div>
+        <h3>Weather in {country.name}</h3>
+        <div>
+          <b>temperature: </b>
+          {weather.temperature}° C
+        </div>
+        <div>
+          <img src={weather.weather_icons} alt="Flag" width="50" height="50" />
+        </div>
+        <div>
+          <b>wind: </b>
+          {weather.wind_speed} kmh direction {weather.wind_dir}
+        </div>
+      </div>
     </>
   );
 };
