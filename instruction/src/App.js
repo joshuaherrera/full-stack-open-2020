04f945f3e971bf34fafa35beed6ef6eb50d54b85
herrjosh <herrjosh@lifetime.oregonstate.edu>
2020-08-22 13:10:22 -0700

    finished changing importance and sending data to json server. on extracting comm...

--- instruction/src/App.js
+++ instruction/src/App.js
@@ -25,10 +25,22 @@ const App = () => {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() < 0.5,
-      id: notes.length + 1,
     };
-    setNotes(notes.concat(noteObject));
-    setNewNote("");
+
+    axios.post("http://localhost:3001/notes", noteObject).then((res) => {
+      setNotes(notes.concat(res.data));
+      setNewNote("");
+    });
+  };
+
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`;
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    axios.put(url, changedNote).then((res) => {
+      setNotes(notes.map((note) => (note.id !== id ? note : res.data)));
+    });
   };
 
   const handleNoteChange = (event) => {
@@ -48,7 +60,11 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
       <form onSubmit={addNote}>
