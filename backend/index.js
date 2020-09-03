180362aa20368cfbaf9273ba73c054ea20fa6877
Joshua Herrera <joshuah@zbrains.net>
2020-09-03 13:10:56 -0700

    added rest client and POST / DELETE handlers

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+// this middleware parses json to a jsobject, then
+// attaches to body property of request
+app.use(express.json());
+
 let notes = [
   {
     id: 1,
@@ -28,6 +32,34 @@ app.get("/", (req, res) => {
 
 app.get("/api/notes", (req, res) => {
   res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const note = notes.find((note) => {
+    // console.log(note.id, typeof note.id, id, typeof id, note.id === id);
+    return note.id === id;
+  });
+  if (note) {
+    res.json(note);
+  } else {
+    //don't update ui since REST APIs are for programmatic use.
+    res.status(404).end();
+  }
+});
+
+app.post("/api/notes", (req, res) => {
+  const note = req.body;
+  console.log(note);
+
+  res.json(note);
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  notes = notes.filter((note) => note.id === id);
+
+  res.status(204).end();
 });
 
 const PORT = 3001;
