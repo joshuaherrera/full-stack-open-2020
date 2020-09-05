774d828a5abb379653f2888834e803c4f50fcebe
herrjosh <herrjosh@lifetime.oregonstate.edu>
2020-09-05 16:13:57 -0700

    updated front end to integrate with backend in dev using proxy

---
+++ instruction/build/precache-manifest.5349af6052ce2e6cde7166c821401cfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f11a11b78ac75c473e41a768885001b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a29a26220cab1604aa7f",
+    "url": "/static/css/main.e1d7813a.chunk.css"
+  },
+  {
+    "revision": "97ee55b723226ca8b6bf",
+    "url": "/static/js/2.f200ff29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f200ff29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a29a26220cab1604aa7f",
+    "url": "/static/js/main.465a4b4a.chunk.js"
+  },
+  {
+    "revision": "c5e8f5ba0b491ea9966c",
+    "url": "/static/js/runtime-main.222d136d.js"
+  }
+]);
